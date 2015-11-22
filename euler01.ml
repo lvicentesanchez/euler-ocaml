db1fbc619759abb13cd31f0faa579ca8a386cada
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-11-22 23:37:49 +0000

    * minor refactoring.

--- euler01.ml
+++ euler01.ml
@@ -1,10 +1,12 @@
 let range a b =
-  let rec aux n acc =
-    if n < a then acc else aux (n - 1) (n :: acc)
-  in aux b [];;
+  let rec loop n acc =
+    if n < a then acc else loop (n - 1) (n :: acc)
+  in loop b [];;
 
 let problem_01 n =
-  let filter x = (x mod 3) = 0 || (x mod 5) = 0
-  in List.fold_left (+) 0 (List.filter filter (range 0 (n - 1)));;
+  let f x =
+    x mod 3 = 0 || x mod 5 = 0
+  in List.fold_left (+) 0 (List.filter f (range 0 (n - 1)));;
 
 print_int (problem_01 1000);;
+
