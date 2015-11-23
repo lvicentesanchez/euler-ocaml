aec620012ac0e94cca39e20fe8633ad87807312e
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-11-23 23:07:13 +0000

    * refactor problem02 using pattern matching.

--- euler02.ml
+++ euler02.ml
@@ -2,11 +2,10 @@ let is_even n =
   n mod 2 = 0;;
 
 let problem_02 limit =
-  let rec fib a b acc =
-    let n = a + b 
-    in if b > limit then acc
-       else if is_even b then fib b n (acc + b)
-       else fib b n acc
+  let rec fib a b acc = match b with
+    | n when n > limit -> acc
+    | n when is_even n -> fib b (a + b) (acc + b)
+    | n                -> fib b (a + b) acc
   in fib 1 1 0;;
 
 print_int (problem_02 4000000);;
