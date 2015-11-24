431bc190b40dbe388c123e7b3f7425bd4e3a9846
Luis Sanchez <luis.sanchez@williamhill.com>
2015-11-24 16:06:15 +0000

    * use core.std as a dependency; rely on ocamlscript to run problem_01.

--- euler01.ml
+++ euler01.ml
@@ -1,12 +1,14 @@
-let range a b =
-  let rec loop n acc =
-    if n < a then acc else loop (n - 1) (n :: acc)
-  in loop b [];;
+#! /usr/bin/env ocamlscript
+Ocaml.ocamlflags := ["-thread"];
+Ocaml.packs := [ "core" ]
+--
+
+open Core.Std
 
 let problem_01 n =
   let f x =
     x mod 3 = 0 || x mod 5 = 0
-  in List.fold_left (+) 0 (List.filter f (range 0 (n - 1)));;
+  in List.fold_left ~init:0 ~f:(+) (List.filter ~f (List.range 0 n));;
 
 print_int (problem_01 1000);;
 
