47a36c2a7827e953934552d03078faf5da86df61
Luis Sanchez <luis.sanchez@williamhill.com>
2015-12-05 04:51:32 +0000

    * remove unnecessary ;;.

--- euler02.ml
+++ euler02.ml
@@ -4,14 +4,14 @@ Ocaml.packs := []
 --
 
 let is_even n =
-  n mod 2 = 0;;
+  n mod 2 = 0
 
 let problem_02 limit =
   let rec fib acc a = function
     | b when b > limit -> acc
     | b when is_even b -> fib (acc + b) b (a + b)
     | b                -> fib acc b (a + b)
-  in fib 0 1 1;;
+  in fib 0 1 1
 
 let () =
-  print_int (problem_02 4000000);;
+  print_int (problem_02 4000000)
